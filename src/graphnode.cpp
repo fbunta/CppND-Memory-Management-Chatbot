0e73431a557d43a7a9f36928fb468be462ce30a7
Finn Bunta <fbunta@belvederetrading.com>
2020-12-26 11:02:22 -0600

    task 4 fix index in graphnode

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -30,7 +30,7 @@ void GraphNode::AddEdgeToParentNode(GraphEdge *edge)
 
 void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> &&edge)
 {
-    _childEdges.push_back(edge);
+    _childEdges.get()->push_back(edge);
 }
 
 //// STUDENT CODE
@@ -54,7 +54,7 @@ GraphEdge *GraphNode::GetChildEdgeAtIndex(int index)
     //// STUDENT CODE
     ////
 
-    return _childEdges[index];
+    return _childEdges.get()[index];
 
     ////
     //// EOF STUDENT CODE
